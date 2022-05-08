0966ecaa3cbc8488c2284a58a2d6bec543e5721c
JanithaChamod <it20002992@my.sliit.lk>
2022-05-09 02:29:44 +0530

    sprint2

--- MiniGarment-Backend/springboot-backend/src/main/java/com/example/demo/Controller/C_MachineController.java
+++ MiniGarment-Backend/springboot-backend/src/main/java/com/example/demo/Controller/C_MachineController.java
@@ -1,5 +1,84 @@
 package com.example.demo.Controller;
 
-public class C_MachineController {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Exception.ResourceNotFoundException;
+import com.example.demo.Model.C_Machine;
+import com.example.demo.Repository.C_MachineRepository;
+
+@CrossOrigin(origins = "http://localhost:3001")
+@RestController
+@RequestMapping("/api/v1")
+public class C_MachineController {
+	
+	@Autowired
+	private C_MachineRepository c_machinerepository;
+	
+	//get all machine
+	
+	@GetMapping("/machine")
+	public List<C_Machine> getAllMachines(){
+		return c_machinerepository.findAll();
+	}
+	
+	//create machine api
+	@PostMapping("/machines")
+	public C_Machine createMachine(@RequestBody C_Machine c_machine) {
+		return c_machinerepository.save(c_machine);
+	}
+	
+	//get machine by Id api
+	@GetMapping("/machines/{machinenumber}")
+	public ResponseEntity<C_Machine> getMachineByMachinenumber(@PathVariable String machinenumber) {
+		
+		C_Machine c_machine = c_machinerepository.findById(machinenumber)
+				.orElseThrow(()-> new ResourceNotFoundException("Machine not exist with machine number :" + machinenumber ));
+		return ResponseEntity.ok(c_machine);
+	}
+	
+	//update matchine api
+	@PutMapping("/machines/{machinenumber}")
+	public ResponseEntity<C_Machine> updateMachine(@PathVariable String machinenumber,@RequestBody C_Machine c_machinemachineDetails){
+		C_Machine c_machine = c_machinerepository.findById(machinenumber)
+				.orElseThrow(()-> new ResourceNotFoundException("Machine not exist with machine number :" + machinenumber ));
+		
+		c_machine.setMachinenumber(c_machinemachineDetails.getMachinenumber());
+		c_machine.setMachinemodel(c_machinemachineDetails.getMachinemodel());
+		c_machine.setDateoflastrepair(c_machinemachineDetails.getDateoflastrepair());
+		c_machine.setDateofnextrepair(c_machinemachineDetails.getDateofnextrepair());
+		c_machine.setReplacedparts(c_machinemachineDetails.getReplacedparts());
+		c_machine.setRepaircharge(c_machinemachineDetails.getRepaircharge());
+		
+		C_Machine updatedMachine = c_machinerepository.save(c_machine);
+		return ResponseEntity.ok(updatedMachine);
+		
+	}
+	
+	//delete machine
+		@DeleteMapping("/machines/{machinenumber}")
+		public ResponseEntity<Map<String, Boolean>> deleteMachine(@PathVariable String machinenumber){
+			
+			C_Machine c_machine = c_machinerepository.findById(machinenumber)
+					.orElseThrow(()-> new ResourceNotFoundException("Machine not exist with machine number :" + machinenumber ));
+			
+			c_machinerepository.delete(c_machine);
+			Map<String, Boolean> response = new HashMap<>();
+			response.put("delete", Boolean.TRUE);
+			return ResponseEntity.ok(response);
+	
+		}
 }
--- MiniGarment-Backend/springboot-backend/src/main/java/com/example/demo/Model/C_Employee.java
+++ MiniGarment-Backend/springboot-backend/src/main/java/com/example/demo/Model/C_Employee.java
@@ -1,5 +1,117 @@
 package com.example.demo.Model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+
 public class C_Employee {
+	
+	@Id
+	@Column(name = "employee_id")
+	private String employeeid;
+	
+	@Column(name = "employee_name")
+	private String employeename;
+	
+	@Column(name = "nic")
+	private int nic;
+	
+	@Column(name = "gender")
+	private String gender;
+	
+	@Column(name = "occupation")
+	private String occupation;
+	
+	@Column(name = "phone_number")
+	private int phonenumber;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "basic_sallary")
+	private int basicsallary;
+	
+	public C_Employee() {
+		
+	}
+	
+	
+	public C_Employee(String employeename, int nic, String gender, String occupation, int phonenumber, String email,
+			String address, int basicsallary) {
+		super();
+		this.employeename = employeename;
+		this.nic = nic;
+		this.gender = gender;
+		this.occupation = occupation;
+		this.phonenumber = phonenumber;
+		this.email = email;
+		this.address = address;
+		this.basicsallary = basicsallary;
+	}
+	public String getEmployeeid() {
+		return employeeid;
+	}
+	public void setEmployeeid(String employeeid) {
+		this.employeeid = employeeid;
+	}
+	public String getEmployeename() {
+		return employeename;
+	}
+	public void setEmployeename(String employeename) {
+		this.employeename = employeename;
+	}
+	public int getNic() {
+		return nic;
+	}
+	public void setNic(int nic) {
+		this.nic = nic;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getOccupation() {
+		return occupation;
+	}
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+	public int getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(int phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getBasicsallary() {
+		return basicsallary;
+	}
+	public void setBasicsallary(int basicsallary) {
+		this.basicsallary = basicsallary;
+	}
+	
+	
+	
+	
 
 }
